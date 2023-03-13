7f23de5e4e8a41f33da5d903c24700ffd7fe3838
egovelox <egovelox@protonmail.com>
2023-03-13 11:22:26 +0100

    update

--- bin/bin/sync_bkms.sh
+++ bin/bin/sync_bkms.sh
@@ -2,7 +2,7 @@
 
 BKMS_DIR="$HOME/bkms"
 
-if ! [ -d  $BKMS_DIR ] then
+if ! [ -d  $BKMS_DIR ]; then
   echo "Warning: $BKMS_DIR folder was not found."
   echo "Create $BKMS_DIR directory and clone inside https://github.com/egovelox/bkms.git"
   echo "Then stow !"
