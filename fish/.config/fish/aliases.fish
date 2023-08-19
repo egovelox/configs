303d8b1b6e256b86c6a9c90f7ecb9b8294aa6c27
egovelox <egovelox@protonmail.com>
2023-08-19 13:25:15 +0200

    aug #3

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -19,10 +19,11 @@
   alias fbuild='$HOME/bin/trigger_jenkins_build.sh'
   alias fdat='hfactory database connect -i'
   alias flog='hfactory logs preview -m'
+
+  alias whr="weechat-hotlist-rs"
   alias sl="weechat-hotlist-rs hotlist | jq '{threads:.priority_1, private:.priority_2, public:.priority_3}'"
   alias sld="weechat-hotlist-rs hotlist -f detailed | jq "
 
-  # same as Command + S
   alias ss='tmux list-windows -a | cut -d" " -f1,2 | while read line; echo $line | sed -e "s/: / /" -e "s/+/ /g" ; end | fzf-tmux -p 30% -- --prompt "switch window: " --no-bold --layout=reverse --height 50% --margin 0% --no-separator --no-info --black --color bg+:black,hl:reverse,hl+:reverse,gutter:black | cut -d " " -f1 | xargs tmux switch -t'
   
   alias fabs='$HOME/DEV/mdn/ftabswitch/app/switch_tab.sh'
@@ -37,8 +38,5 @@
 
   alias fedor="ssh -i "$HOME/.ssh/fedor" -p8887 cuncdev@192.168.2.203"
   alias fedor_r="ssh -i "$HOME/.ssh/fedor" -p8887 cuncdev@78.194.17.20"
-  alias run="/Users/maxime.richard/DEV/poc_product_search/poc_es_flow/run"
-alias run="/Users/maxime.richard/DEV/poc_product_search/poc_es_flow/run"
-alias run="/Users/maxime.richard/DEV/poc_product_search/poc_es_flow/run"
-alias run="/Users/maxime.richard/DEV/poc_product_search/poc_es_flow/run"
-alias run="/Users/maxime.richard/DEV/poc_product_search/poc_es_flow/run"
+
+  alias run="$HOME/DEV/poc_product_search/poc_es_flow/run"
