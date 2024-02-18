020b4cebbc3c538dd4917f547dc3e3c6ecdcf529
egovelox <egovelox@protonmail.com>
2024-02-18 13:21:14 +0100

    Disable oil at startup

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -49,6 +49,7 @@ local plugins = {
     config = function()
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
       require("oil").setup({
+        default_file_explorer = false,
         columns = {
           "icon",
         },
