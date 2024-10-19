64fb806831c0841b74aa63a4ec47665e01969d9f
egovelox <egovelox@protonmail.com>
2024-10-19 22:56:47 +0200

    Nvim add nvim-rooter

--- nvim/.config/nvim/lua/user/plugins/vim-rooter.lua
+++ nvim/.config/nvim/lua/user/plugins/vim-rooter.lua
@@ -1,9 +1,11 @@
 return {
-  {
-    "airblade/vim-rooter",
-    init = function()
-      vim.g.rooter_patterns = { '.git' }
-    end,
-  },
+  -- auto-cd to root of git project
+	-- removed 'airblade/vim-rooter'
+	{
+		'notjedi/nvim-rooter.lua',
+		config = function()
+			require('nvim-rooter').setup()
+		end
+	},
 }
 
