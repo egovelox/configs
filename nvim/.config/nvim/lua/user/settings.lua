f4839e44984661027d1514664686d6d96b55ad9a
egovelox <egovelox@protonmail.com>
2024-10-19 22:54:34 +0200

    Improve nvim settings: relative numbers, vertical line, etc

--- nvim/.config/nvim/lua/user/settings.lua
+++ nvim/.config/nvim/lua/user/settings.lua
@@ -1,6 +1,18 @@
+-- always set leader first!
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
+vim.g.mapleader = " "
+
+-- never ever folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'manual'
+vim.opt.foldlevelstart = 99
+
 -- first enable nvim global status bar
 vim.opt.laststatus = 3
-vim.g.mapleader = " "
+
+-- sweet sweet relative line numbers
+vim.opt.relativenumber = true
+-- and show the absolute line number for the current line
 vim.opt.number = true
 
 vim.opt.cursorline = true
@@ -25,3 +37,21 @@ vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
 vim.o.termguicolors = true
+
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 2
+
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = 'yes'
+
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = '80'
+--- except in Rust where the rule is 100 characters
+vim.api.nvim_create_autocmd('Filetype', { pattern = 'rust', command = 'set colorcolumn=100' })
+
+-- show more hidden characters
+-- also, show tabs nicer
+vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
