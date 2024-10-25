d74f138cc596ec1aded1042024ccdaa3b672e599
egovelox <egovelox@protonmail.com>
2024-10-25 14:57:09 +0200

    improve tmux-status.conf

--- nvim/.config/nvim/lua/user/plugins/varia.lua
+++ nvim/.config/nvim/lua/user/plugins/varia.lua
@@ -12,22 +12,6 @@ return {
 		end
 	},
   {
-    'numToStr/Navigator.nvim',
-    keys = {
-      { '<C-s>h', '<CMD>NavigatorLeft<CR>' },
-      { '<C-s>l', '<CMD>NavigatorRight<CR>' },
-      { '<C-s>k', '<CMD>NavigatorUp<CR>' },
-      { '<C-s>j', '<CMD>NavigatorDown<CR>' },
-      -- { 't', '<C-s>h', '<CMD>NavigatorLeft<CR>' },
-      -- { 't', '<C-s>l', '<CMD>NavigatorRight<CR>' },
-      -- { 't', '<C-s>k', '<CMD>NavigatorUp<CR>' },
-      -- { 't', '<C-s>j', '<CMD>NavigatorDown<CR>' },
-    },
-    config = function()
-        require('Navigator').setup()
-    end
-  },
-  {
     'ilAYAli/scMRU.nvim',
     -- this plugin can really slow down the whole nvim startup
     -- make it lazy !
